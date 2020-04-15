d8c32355af1ceb58d6682b143fb79123de65d908
Vattekar <42180319+erikvatt@users.noreply.github.com>
2020-04-15 13:26:10 +0200

    mount api on subpath

--- src/digdir_api/main.py
+++ src/digdir_api/main.py
@@ -3,9 +3,7 @@ import os
 
 from dotenv import load_dotenv
 from fastapi import FastAPI
-from fastapi.staticfiles import StaticFiles
 from digdir_api.routers import health, terms, dataset
-from fastapi.openapi.docs import get_swagger_ui_html
 
 try:
     os.environ["PRODUCTION"]
@@ -13,8 +11,10 @@ except KeyError:
     logging.warning("Getting env variables from .env file")
     load_dotenv()
 
-app = FastAPI(docs_url="/digdir-api/docs")
+app = FastAPI()
+subapi = FastAPI(docs_url="/docs", openapi_prefix="/digdir-api")
 
-app.include_router(health.router)
-app.include_router(terms.router)
-app.include_router(dataset.router)
+app.mount("/digdir-api", subapi)
+subapi.include_router(health.router)
+subapi.include_router(terms.router)
+subapi.include_router(dataset.router)
--- src/digdir_api/routers/terms.py
+++ src/digdir_api/routers/terms.py
@@ -6,7 +6,7 @@ from digdir_api.collections.terms import TermCollection
 router = APIRouter()
 
 
-@router.get("/digdir-api/terms")
+@router.get("/terms")
 async def get_all_terms():
     terms = await TermCollection().create()
     return JSONResponse(status_code=status.HTTP_200_OK, content=terms)
