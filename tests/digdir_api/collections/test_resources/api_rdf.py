53be88eb16bc4bcc126f63e311d2e27b276d3759
Vattekar <42180319+erikvatt@users.noreply.github.com>
2021-01-13 14:07:16 +0100

    bump datacatalogtordf

--- tests/digdir_api/collections/test_resources/api_rdf.py
+++ tests/digdir_api/collections/test_resources/api_rdf.py
@@ -1,16 +1,14 @@
 API_RDF = """@prefix dcat: <http://www.w3.org/ns/dcat#> .
 @prefix dct: <http://purl.org/dc/terms/> .
-@prefix ns1: <http://xmlns.com/foaf/0.1/> .
+@prefix foaf: <http://xmlns.com/foaf/0.1/> .
 
-<API.DatakatalogAPI> a dcat:DataService ;
-    dct:description "Datakatalog api for s3 og elastic search"@nb ;
-    dct:publisher <https://organization-catalogue.fellesdatakatalog.digdir.no/organizations/889640782> ;
-    dct:title "Datakatalog API"@nb ;
-    dcat:endpointDescription <https://data.nav.no/api/swagger/ui> ;
-    dcat:endpointURL <https://data.nav.no/api> .
+<API.Arbeidsplassen> a dcat:DataService ;
+    dct:description "Stillingssøk på Arbeidsplassen inneholder en oversikt over de fleste aktive ledige stillinger. Stillingssøket inneholder både stillinger som er registrert direkte hos NAV samt hentet inn fra en rekke nettsteder Stillingsdata meldes og registreres i NAV basert på (Lov om arbeidsmarkedstjenester § 7,2004). Ikke alle stillinger som blir meldt til NAV blir publisert på Arbeidsplassen. Du finner dokumentasjon av applikasjonsgrensesnittet på https://github.com/navikt/pam-public-feed"@nb ;
+    dct:title "Arbeidsplassen"@nb ;
+    dcat:endpointDescription <https://arbeidsplassen.nav.no/public-feed/swagger> ;
+    dcat:endpointURL <https://arbeidsplassen.nav.no/public-feed/api/v1> .
 
 <https://data.nav.no> a dcat:Catalog ;
-    dct:publisher <https://organization-catalogue.fellesdatakatalog.digdir.no/organizations/889640782> ;
     dct:title "NAV åpne APIer"@nb ;
-    dcat:service <API.DatakatalogAPI> ;
-    ns1:homepage <https://data.nav.no> .\n\n"""
+    dcat:service <API.Arbeidsplassen> ;
+    foaf:homepage <https://data.nav.no> .\n\n"""
