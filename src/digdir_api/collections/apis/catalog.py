f575ae617e7ced8ceb711740f9af54fce86291a0
Vattekar <42180319+erikvatt@users.noreply.github.com>
2021-01-18 14:40:35 +0100

    remove debug

--- src/digdir_api/collections/apis/catalog.py
+++ src/digdir_api/collections/apis/catalog.py
@@ -11,9 +11,6 @@ def create_catalog():
     _add_dataservices(catalog)
     _add_mandatory_catalog_props(catalog)
     _add_optional_catalog_props(catalog)
-
-    print(catalog.to_rdf().decode())
-
     return catalog.to_rdf().decode()
 
 
@@ -22,7 +19,6 @@ def _add_dataservices(catalog: Catalog, size: int = 10000):
     for es_hit in es_hits:
         data_service = api.create_api(es_hit["_source"])
         catalog.services.append(data_service)
-        break
 
 
 def _add_mandatory_catalog_props(catalog: Catalog) -> None:
